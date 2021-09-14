3050e7de4034eb7668f497375c923d5463e332e6
Vitalii <puzanov.vitaly@gmail.com>
2021-09-14 14:33:34 +0300

    async 4 task done

--- Async/Г/js.js
+++ Async/Г/js.js
@@ -1,11 +1,44 @@
-function delay(duration){ 
-    return new Promise(resolve => setTimeout(resolve, duration));
+class HttpError extends Error {
+  constructor(response) {
+    super(`${response.status} for ${response.url}`);
+    this.name = 'HttpError';
+    this.response = response;
+  }
+}
+
+async function loadJson(url) {
+  let response = await fetch(url);
+  if (response.status == 200) {
+    return response.json();
+  } else {
+    throw new HttpError(response);
+  }
+}
+
+
+async function demoGithubUser() {
+
+  let user;
+  while(true) {
+    let name = prompt("Login", "iliakan");
+
+    try {
+      user = await loadJson(`https://api.github.com/users/${name}`);
+      break; 
+    } catch(err) {
+      if (err instanceof HttpError && err.response.status == 404) {
+       
+        alert("Used not exisit");
+      } else {
+        throw err;
+      }
     }
-    
-    
-    function logHi(){ 
-    console.log('Hi');
-    } 
-    
-    delay(2000).then(logHi)
+  }
+
+
+  alert(`Fulllname: ${user.name}.`);
+  return user;
+}
+
+demoGithubUser();  
     
