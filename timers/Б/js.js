688c926618e45fc770021cf079e4e5d1cdb15930
Vitalii <puzanov.vitaly@gmail.com>
2021-09-14 10:19:09 +0300

    rework timers

--- timers/Б/js.js
+++ timers/Б/js.js
@@ -1,17 +1,23 @@
-//setup before functions
-let typingTimer;                //timer identifier  //time in ms (5 seconds)
+const input1 = document.getElementById("input1");
+const input2 = document.getElementById("input2");
+    const INPUD_ID1 = "input1";
+    const INPUD_ID2 = "input2";
+    let keyupTimer;
 
+    function keyUpEvent(inputId) {
+      if (keyupTimer) {
+        clearTimeout(keyupTimer);
+      }
 
-//on keyup, start the countdown
-let text = document.getElementById('myInput');
-text.addEventListener('keyup', () => {
-    clearTimeout(typingTimer);
-    if (text.value) {
-        typingTimer = setTimeout(doneTyping, 5000);
+      keyupTimer = setTimeout(copy, 1000, inputId);
     }
-});
 
-//user is "finished typing," do something
-function doneTyping () {
-    //do something
-}
+    function copy(inputId) {
+      console.log(inputId);
+      if (inputId === INPUD_ID1) {
+        input2.value = input1.value;
+      }
+      if (inputId === INPUD_ID2) {
+        input1.value = input2.value;
+      }
+    }
