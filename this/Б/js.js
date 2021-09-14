f25f836ec623735c47f7088b37a63c28a934d7fe
Vitalii <puzanov.vitaly@gmail.com>
2021-09-14 10:03:02 +0300

    rework 2 task

--- this/Б/js.js
+++ this/Б/js.js
@@ -7,9 +7,22 @@ function add (op1,op2){
     this.name=this.name || 'Human'
 return this.name + ' can count to ' + (op1 + op2);
 }
+
+Function.prototype.myOwnBind = function(robot) {
+  const addFunction = this;
+  return function bindFunction() {
+      return addFunction.apply(
+          robot, // robot
+          [].slice.call(arguments) // [1, 2]
+      );
+  };
+}
+
 console.log(add(0,1));
 const voltron = new Robot ('voltron')
-const voltronfn = add.bind(voltron)
+const voltronfn = add.myOwnBind(voltron)
+
+
 console.log(voltronfn(1,2));
 console.log(voltronfn(20,30));
 
